3de8710484daab918cd3e9615f8cf5afde19f835
efeaizesogie <efeaizesogie@gmail.com>
2023-09-20 19:13:47 +0100

    edited gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,21 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "./../firebase";
 import "./../styles/gallery.css";
-
 import React, { useState, useEffect } from "react";
 
-function ImageCard({ image }) {
+function ImageCard({ image, index, onDragStart, onDragOver, onDrop, loading }) {
   return (
-    <div className="image-card">
-      <img src={image.webformatURL} alt={image.tags} />
-      <div className="image-details">
-        <div className="tags">
-          {image.tags.split(",").map((tag) => (
-            <span key={tag.trim()} className="tag">
-              {tag.trim()}
-            </span>
-          ))}
-        </div>
-        <p className="description">{image.type}</p>
-      </div>
+    <div
+      className={`image-card ${loading ? "skeleton-card" : ""}`}
+      draggable="true"
+      onDragStart={(e) => onDragStart(e, index)}
+      onDragOver={(e) => onDragOver(e)}
+      onDrop={(e) => onDrop(e, index)}
+    >
+      {loading ? (
+        <div className="skeleton-loader"></div>
+      ) : (
+        <>
+          <img src={image.webformatURL} alt={image.tags} />
+          <div className="image-details">
+            <div className="tags">
+              {image.tags.split(",").map((tag) => (
+                <span key={tag.trim()} className="tag">
+                  {tag.trim()}
+                </span>
+              ))}
+            </div>
+            <p className="description">{image.type}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
@@ -25,14 +39,52 @@ function Gallery() {
   const [loading, setLoading] = useState(true);
   const [searchTags, setSearchTags] = useState("");
   const [filteredImages, setFilteredImages] = useState([]);
+  const [draggedImageIndex, setDraggedImageIndex] = useState(null);
+  const container = `container ${loading ? "skeleton-loader" : "image-grid"}`;
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+        console.log("Signed out successfully");
+      })
+      .catch((error) => {});
+  };
 
   const handleTagInputChange = (e) => {
     setSearchTags(e.target.value);
   };
 
+  const handleDragStart = (e, index) => {
+    setDraggedImageIndex(index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, targetIndex) => {
+    e.preventDefault();
+
+    if (draggedImageIndex === null) {
+      return;
+    }
+
+    const updatedImages = [...filteredImages];
+    const draggedImage = updatedImages[draggedImageIndex];
+    updatedImages.splice(draggedImageIndex, 1);
+    updatedImages.splice(targetIndex, 0, draggedImage);
+
+    setFilteredImages(updatedImages);
+    setDraggedImageIndex(null);
+  };
+
   useEffect(() => {
     async function fetchImagesFromApi() {
       setLoading(true);
+
       try {
         const response = await fetch(
           "https://pixabay.com/api/?key=25872291-f20fe8f2ed821c6a1ed214f86&q"
@@ -44,9 +96,9 @@ function Gallery() {
 
         const data = await response.json();
         setImages(data.hits || []);
-        setLoading(false);
       } catch (error) {
         console.error("Error fetching images:", error);
+      } finally {
         setLoading(false);
       }
     }
@@ -60,11 +112,20 @@ function Gallery() {
     );
 
     setFilteredImages(filtered);
+    setLoading(false);
   }, [searchTags, images]);
 
   return (
     <div className="gallery">
-      <h1>Image Gallery</h1>
+      <div className="nav">
+        <p className="logo">
+          re<span>Drag</span>
+        </p>
+        <h1>Image Gallery</h1>
+        <p className="logout" onClick={handleLogout}>
+          LogOut
+        </p>
+      </div>
       <input
         type="text"
         placeholder="Search by tag"
@@ -72,11 +133,23 @@ function Gallery() {
         onChange={handleTagInputChange}
       />
       {loading ? (
-        <div className="loader">Loading...</div>
+        <div className="loader">
+          <div className="skeleton-loader"></div>
+          <div className="skeleton-loader"></div>
+          <div className="skeleton-loader"></div>
+        </div>
       ) : (
         <div className="image-grid">
-          {filteredImages.map((image) => (
-            <ImageCard key={image.id} image={image} />
+          {filteredImages.map((image, index) => (
+            <ImageCard
+              key={image.id}
+              image={image}
+              index={index}
+              onDragStart={handleDragStart}
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+              loading={loading}
+            />
           ))}
         </div>
       )}
